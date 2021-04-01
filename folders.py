fc43af3fe04e8adf589a7a8ae42be981e6c5bf57
katiepreed <75800887+katiepreed@users.noreply.github.com>
2021-04-01 22:47:58 +0100

    Update folders.py

--- folders.py
+++ folders.py
@@ -3,7 +3,7 @@
 # print them in the form of a tree with children arranged lexicographically.
 
 # INPUT
-# The first line contains integer n (1 =< n =< 100) , the number of paths
+# The first line contains integer n (1 <= n <= 100) , the number of paths
 # Next n lines contain the paths themselves, one per line
 # all paths are different, consist of small Latin letters and slashes, have lengths fom 1 to 100
 # no path starts with a slash, ends with a slash, or contains two or more slashes in a row
