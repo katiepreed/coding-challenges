c9e4b4dbfccfc90e714471859098fb76294fd1e5
Katie Polyakova Reed <75800887+katiepreed@users.noreply.github.com>
2021-05-05 16:05:41 +0100

    Create game of chance.py

---
+++ game of chance.py
@@ -0,0 +1,14 @@
+# user can bet on any number from 0 to 30.
+# If it is an even number they get x2 money back
+# if it is a multiple of 10 they get x3 money back
+# if it is a prime number they get x5 money back
+# if number below 5 they get x2 bonus
+
+# user guesses number and if it is correct they get a bonus
+# e.g. 20  means a bonus of 2 x 3 = x6 their money
+
+# allow user to enter the amount they want to bet and work out the bonus they recieve
+# store the user's current balance and stop them from betting if they have no money left.
+# the user cannot enter into a negative amount of cash
+# input should be between 1 and 10 units of currency
+# allow multiple bets on different numbers
